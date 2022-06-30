feaceada969a2eb039fc1128e9ca4b6bc5b4c924
renzhenming <1565766177@qq.com>
2022-06-30 19:19:31 +0800

    glide

--- app/src/main/java/com/rzm/testapplication/glide/GlideActivity.java
+++ app/src/main/java/com/rzm/testapplication/glide/GlideActivity.java
@@ -18,6 +18,7 @@ public class GlideActivity extends AppCompatActivity {
         Glide.with(this).load("https://img0.baidu.com/it/u=3798217922,3880088897&fm=253&fmt=auto&app=120&f=JPEG?w=889&h=500")
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
                 .skipMemoryCache(true)
+                .override(100,100)
                 .into((ImageView) findViewById(R.id.image));
     }
 }
